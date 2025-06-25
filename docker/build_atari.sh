0b2413e8e149edb70a8491116007b6f1338f8b73
milad <mi.aghajohari@gmail.com>
2025-06-25 18:38:41 -0400

    clean unnecessary files and folders

--- docker/build_atari.sh
+++
@@ -1,7 +0,0 @@
-docker build \
-    --build-arg REQUIREMENTS_FILE=requirements/requirements_atari.txt \
-    --build-arg UID=$(id -u ${USER}) \
-    --build-arg USERNAME=$(whoami) \
-    -f docker/Dockerfile \
-    -t pqn-atari \
-    .
--- docker/run_atari.sh
+++
@@ -1,7 +0,0 @@
-gpu=0
-docker run -it --rm --gpus=\"device=${gpu}\" --ipc=host \
- -v $(pwd):/app \
- -w /app \
- --name "pqn-${gpu//,/-}" \
- pqn-atari \
- bash
